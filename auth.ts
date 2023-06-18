0ddec66559b20a362da2ee743fd32ddd8cc55940
Carlos Ziegler <carlos.ziegler@gmail.com>
2023-06-18 10:23:28 +0200

    🔥 refactor(auth.ts): remove unused code and comments 🐛 fix(auth.ts): update jwt callback to handle undefined token and log token name instead of entire token object

--- auth.ts
+++ auth.ts
@@ -3,30 +3,21 @@ import GitHub from 'next-auth/providers/github'
 
 export const {
   handlers: { GET, POST },
-  auth,
-  CSRF_experimental
+  auth
+  // CSRF_experimental
   // @ts-ignore
 } = NextAuth({
   // @ts-ignore
   providers: [GitHub],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }: any) {
-      return true
-    },
     // @ts-ignore
     jwt: async ({ token, profile }) => {
-      console.log('jwt - token', token, profile)
+      console.log('jwt - token', token?.name)
       if (profile?.id) {
         token.id = profile.id
         token.image = profile.picture
       }
       return token
-    },
-    session: async ({ session, token, user }: any) => {
-      console.log('session', session, token, user)
-      session.user.id = token.id
-      session.user.image = token.image
-      return session
     }
     // @TODO
     // authorized({ request, auth }: any) {
