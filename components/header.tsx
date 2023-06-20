44ab141d28101b2b6797c406eeea18fae4fd26ae
Carlos Ziegler <carlos.ziegler@gmail.com>
2023-06-20 20:38:01 +0200

    feat: delete log

--- components/header.tsx
+++ components/header.tsx
@@ -21,6 +21,7 @@ import { LoginButton } from '@/components/login-button'
 
 export async function Header() {
   const session = await auth()
+
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
@@ -55,6 +56,7 @@ export async function Header() {
           )}
         </div>
       </div>
+
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
